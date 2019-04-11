eaf5a91aee0cde07d1920daa8015bae88a0b44f5
黄兴隆 <1292028314@qq.com>
2019-04-11 09:58:29 +0800

    build docs

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,16 +21,16 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Bytom PHP API文档', // Title for your website.
-  tagline: 'A website for Bytom PHP SDK',
-  url: 'http://bytomphp.github.io', // Your website URL
+  title: 'Bytom Java API文档', // Title for your website.
+  tagline: 'A website for Bytom Java SDK',
+  url: 'https://bytomfans.github.io/java-docs/', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'Bytom PHP SDK API',
+  projectName: 'Bytom Java SDK API',
   organizationName: 'Bytom',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
